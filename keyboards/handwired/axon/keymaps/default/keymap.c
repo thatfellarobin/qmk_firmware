3c93e2048c5f1334e7bc211843bc3e6ac9e1ee77
thatfellarobin <Robin.Liu.831@gmail.com>
2022-03-05 10:00:00 -0500

    fix layer 4 for macos mods

--- keyboards/handwired/axon/keymaps/default/keymap.c
+++ keyboards/handwired/axon/keymaps/default/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_DEL,
 		KC_TAB,  KC_CAPS, KC_UP,   KC_NO,    KC_PGUP,  KC_HOME, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, KC_ENTER,
 		KC_LSFT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,  KC_END,  KC_LBRC, KC_RBRC, KC_SLSH, KC_BSLS, KC_TRNS,
-		         KC_LALT, KC_LCTL,           KC_SPACE,          KC_TRNS,          KC_RGUI, TO(0)
+		         KC_LALT, KC_LGUI,           KC_SPACE,          KC_TRNS,          KC_RCTL, TO(0)
 	),
 
 	LAYOUT_default(//Layer 5
